8e6cd6332664bf75e8bb18325585ca3bbf4ca893
satheesh22g <satheeshgajula22.com>
2023-12-10 00:23:31 +0530

    add plagiarism app

--- extract/urls.py
+++ extract/urls.py
@@ -1,6 +1,8 @@
 # urls.py
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('extract/', views.home, name='home'),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('pdf-extraction/', views.pdf_extraction, name='pdf_extraction'),
     # Other URLs within your app if needed
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- utility/settings.py
+++ utility/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 
 # Quick-start development settings - unsuitable for production
@@ -44,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'extract'
+    'extract',
+    'plagiarism',
 
 ]
 
